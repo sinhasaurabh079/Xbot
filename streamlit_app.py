e11d338b1882402b6499698819d4d1402f97bc32
Saurabh Sinha <sinhasaurabh079@gmail.com>
2025-03-28 19:30:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,6 @@ import os
 import warnings
 import logging
 import streamlit as st
-from dotenv import load_dotenv
-
-load_dotenv()
-
 from langchain_groq import ChatGroq
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
